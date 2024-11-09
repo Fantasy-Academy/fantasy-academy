b5668ae337c65c69778425e98e07b1be8de77603
skrzeczek <h.skrzeczek@gmail.com>
2024-11-09 17:18:00 +0100

    frontend: added CBA for forgotPassword

--- frontend/app/forgotPassword/page.tsx
+++ frontend/app/forgotPassword/page.tsx
@@ -2,13 +2,15 @@
 
 import React from 'react';
 import { useRouter } from 'next/navigation';
+import InputField from '../../components/common/InputField';
+import Button from '../../components/common/Btn';
+import LinkBtn from '../../components/common/LinkBtn';
 
 export default function ForgotPassword() {
   const router = useRouter();
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     alert("Password reset link has been sent to your email.");
     router.push('/login');
   };
@@ -23,38 +25,14 @@ export default function ForgotPassword() {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-900">
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                required
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-800 sm:text-sm"
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800"
-            >
-              Send Password Reset Link
-            </button>
-          </div>
+          <InputField id="email" label="Email address" type="email" autoComplete="email" required />
+          <Button type="submit" text="Send Request" />
         </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Remembered your password?{' '}
-          <a href="/login" className="font-semibold text-gray-800 hover:text-gray-600">
-            Login
-          </a>
-        </p>
+        <div className="mt-10 flex justify-center text-sm text-gray-500">
+          <span className="mr-1">Remembered your password?</span>
+          <LinkBtn link="/login" text="Login" />
+        </div>
       </div>
     </div>
   );
