326cd7573f985616567a34b6a9950ca0ddef3129
skrzeczek <h.skrzeczek@gmail.com>
2024-11-09 17:12:20 +0100

    frontend: added padding to leaderboard

--- frontend/app/leaderboard/page.tsx
+++ frontend/app/leaderboard/page.tsx
@@ -11,25 +11,27 @@ const mockUsers = [
 
 const UserTable: React.FC = () => {
     return (
-        <div className="overflow-x-auto">
-            <div className="min-w-full bg-white  shadow-md">
-                <div className="flex justify-between p-4 bg-gray-800 text-white ">
-                    <div className="flex-1 text-left">Nickname</div>
-                    <div className="flex-1 text-left">Rank</div>
-                    <div className="flex-1 text-left">Rounds Played</div>
-                    <div className="flex-1 text-left">Overall Placement</div>
-                </div>
-                {mockUsers.map((user, index) => (
-                    <div 
-                        key={index} 
-                        className={`flex justify-between p-4 border-b ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}
-                    >
-                        <div className="flex-1">{user.nickname}</div>
-                        <div className="flex-1">{user.rank}</div>
-                        <div className="flex-1">{user.roundsPlayed}</div>
-                        <div className="flex-1">{user.overallPlacement}</div>
+        <div className="px-4 py-8">
+            <div className="overflow-x-auto bg-white shadow-md rounded">
+                <div className="min-w-full">
+                    <div className="flex justify-between p-4 bg-gray-800 text-white">
+                        <div className="flex-1 text-left">Nickname</div>
+                        <div className="flex-1 text-left">Rank</div>
+                        <div className="flex-1 text-left">Rounds Played</div>
+                        <div className="flex-1 text-left">Overall Placement</div>
                     </div>
-                ))}
+                    {mockUsers.map((user, index) => (
+                        <div 
+                            key={index} 
+                            className={`flex justify-between p-4 border-b ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}
+                        >
+                            <div className="flex-1">{user.nickname}</div>
+                            <div className="flex-1">{user.rank}</div>
+                            <div className="flex-1">{user.roundsPlayed}</div>
+                            <div className="flex-1">{user.overallPlacement}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
