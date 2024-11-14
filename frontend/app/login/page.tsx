352f1b04abedd70194fc5d5cdc853d6c9c56ff81
skrzeczek <h.skrzeczek@gmail.com>
2024-11-14 15:39:34 +0100

    frontend: merged changes

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -17,7 +17,6 @@ const Login = () => {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <LoginForm />
-        <LoginForm  />
         <div className="mt-10 flex justify-center text-sm text-gray-500">
           <span className="mr-1">Don&apos;t have an account?</span>
           <LinkBtn link="/signup" text="Sign Up" />
