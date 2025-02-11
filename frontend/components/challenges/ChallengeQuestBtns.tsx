8ec6757314aaab8ce92cf10eca40c5b5a39f7d6f
skrzeczek <h.skrzeczek@gmail.com>
2025-02-11 23:43:15 +0100

    frontend: fixed quest btn text contrast

--- frontend/components/challenges/ChallengeQuestBtns.tsx
+++ frontend/components/challenges/ChallengeQuestBtns.tsx
@@ -5,12 +5,12 @@ const ChallengeQuestBtns = () => {
     return (
         <div className="flex flex-col w-full gap-4 pt-6">
             <div className="flex flex-row w-full gap-4">
-                <Btn text="Answer A" className="w-full bg-goldenYellow" />
-                <Btn text="Answer B" className="w-full bg-goldenYellow" />
+                <Btn text="Answer A" className="w-full bg-goldenYellow text-blueBlack" />
+                <Btn text="Answer B" className="w-full bg-goldenYellow text-blueBlack" />
             </div>
             <div className="flex flex-row w-full gap-4">
-                <Btn text="Answer C" className="w-full bg-goldenYellow" />
-                <Btn text="Answer D" className="w-full bg-goldenYellow" />
+                <Btn text="Answer C" className="w-full bg-goldenYellow text-blueBlack" />
+                <Btn text="Answer D" className="w-full bg-goldenYellow text-blueBlack" />
             </div>
         </div>
     );
