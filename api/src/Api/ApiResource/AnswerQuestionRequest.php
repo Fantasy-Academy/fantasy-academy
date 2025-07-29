53e468083923c33acbc0656175a3b94a70429ba6
Jan Mikeš <j.mikes@me.com>
2025-07-29 22:39:43 +0200

    API: POC - Answer question

--- api/src/Api/ApiResource/AnswerQuestionRequest.php
+++ api/src/Api/ApiResource/AnswerQuestionRequest.php
@@ -4,34 +4,35 @@ declare(strict_types=1);
 
 namespace FantasyAcademy\API\Api\ApiResource;
 
-use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Put;
 use FantasyAcademy\API\Api\Processor\AnswerQuestionProcessor;
-use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Uid\Uuid;
 
 #[ApiResource(
     shortName: 'Answer question',
 )]
 #[Put(
-    uriTemplate: '/questions/{id}/answer',
+    uriTemplate: '/questions/answer',
     status: 204,
     security: "is_granted('IS_AUTHENTICATED_FULLY')",
     input: self::class,
     output: false,
     messenger: 'input',
+    read: false,
     processor: AnswerQuestionProcessor::class,
 )]
 readonly final class AnswerQuestionRequest
 {
     /**
-     * @param null|array<string> $selectedChoiceIds
-     * @param null|array<string> $orderedChoiceIds
+     * @param null|array<Uuid> $selectedChoiceIds
+     * @param null|array<Uuid> $orderedChoiceIds
      */
     public function __construct(
+        public Uuid $questionId,
         public null|string $textAnswer = null,
         public null|float $numericAnswer = null,
-        public null|UuidInterface $selectedChoiceId = null,
+        public null|Uuid $selectedChoiceId = null,
         public null|array $selectedChoiceIds = [],
         public null|array $orderedChoiceIds = [],
     ) {}
