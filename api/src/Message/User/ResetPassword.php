3d2ed614d29be9a771aabdbc29ba96423d44babf
Jan Mikeš <j.mikes@me.com>
2025-08-01 23:12:52 +0200

    API: Reset password

--- api/src/Message/User/ResetPassword.php
+++ api/src/Message/User/ResetPassword.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace FantasyAcademy\API\Message\User;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Put;
+
+#[ApiResource(
+    shortName: 'Forgotten password - reset',
+)]
+#[Put(
+    uriTemplate: '/forgotten-password/reset',
+    status: 204,
+    input: self::class,
+    output: false,
+    messenger: 'input',
+    read: false,
+)]
 readonly final class ResetPassword
 {
     public function __construct(
-        public string $token,
-        public string $newPlainTextPassword,
+        public string $code,
+        public string $newPassword,
     ) {
     }
 }
