0b7371d0f294462829ece39c3a3dfde47d4c5d98
Jan Mikeš <j.mikes@me.com>
2025-08-30 14:36:16 +0200

    API: Answers in response

--- api/src/Value/Question.php
+++ api/src/Value/Question.php
@@ -37,6 +37,8 @@ readonly final class Question
         public null|ChoiceQuestionConstraint $choiceConstraint,
         public null|DateTimeImmutable $answeredAt,
         public null|Answer $answer,
+        public null|Answer $myAnswer,
+        public null|Answer $correctAnswer,
     ) {}
 
     /**
@@ -68,6 +70,8 @@ readonly final class Question
             choiceConstraint: $choiceConstraint,
             answeredAt: $row['answered_at'] !== null ? new DateTimeImmutable($row['answered_at']) : null,
             answer: $row['answered_at'] !== null ? Answer::fromArray($row) : null,
+            myAnswer: $row['answered_at'] !== null ? Answer::fromArray($row) : null,
+            correctAnswer: null,
         );
     }
 }
