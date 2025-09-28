5c03c3c50486f1e736e734ef073adbd21c4e2bd0
skrzeczek <h.skrzeczek@gmail.com>
2025-09-28 10:38:43 +0200

    frontend: added VITE_GA_ID into .env file, GA has been implemented in index.html and main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,15 @@ import './index.css'
 import './styles/tailwind.css';
 
 
+router.afterEach((to) => {
+  if (import.meta.env.PROD && window.gtag) {
+    window.gtag('config', import.meta.env.VITE_GA_ID, {
+      page_path: to.fullPath,
+      page_title: to.name || document.title,
+    });
+  }
+});
+
 const app = createApp(App);
 app.use(router);                
 app.mount('#app');
